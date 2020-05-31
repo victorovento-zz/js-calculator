c1be10a66e6f1acca542b08dd90ec515979d1057
Victor Omar Vento <39925070+victorovento@users.noreply.github.com>
2020-05-31 18:57:43 -0400

    Add files via upload

---
+++ README.md
@@ -0,0 +1,13 @@
+# Calculadora simple
+
+Calculadora escrita completamente sin usar ningun tipo de frameworks, libreria externa, etc.
+Solo he usado HTML, CSS y JavaScript.
+
+Esta calculadora es parte de mi formacion autodidacta.
+Puede tener errores hasta por gusto porque empece a aprender web practicamente el otro dia asi que no la recomiendo para el uso diario
+
+::smiley::
+
+Si te mandaron a hacer una calculadora en 3er año de informatica etc 
+forkea esta calculadora de mierda y presentala a lo mejor cojes 3
+:)
---
+++ src/js/operations.js
@@ -0,0 +1,288 @@
+document.getElementById("pantalla").innerText = "0";
+
+const write1 = () => {
+
+    if (document.getElementById("pantalla").innerText.length <= 12) {
+        if (document.getElementById("pantalla").innerText != 0) {
+            let actual = document.getElementById("pantalla").innerText;
+            let concat = document.getElementById("1").innerText;
+            document.getElementById("pantalla").innerText = actual + concat;
+        } else {
+            let actual = "";
+            let concat = document.getElementById("1").innerText;
+            document.getElementById("pantalla").innerText = actual + concat;
+        }
+    }
+}
+
+const write2 = () => {
+
+    if (document.getElementById("pantalla").innerText.length <= 12) {
+        if (document.getElementById("pantalla").innerText != 0) {
+            let actual = document.getElementById("pantalla").innerText;
+            let concat = document.getElementById("2").innerText;
+            document.getElementById("pantalla").innerText = actual + concat;
+        } else {
+            let actual = "";
+            let concat = document.getElementById("2").innerText;
+            document.getElementById("pantalla").innerText = actual + concat;
+        }
+    }
+}
+
+const write3 = () => {
+
+    if (document.getElementById("pantalla").innerText.length <= 12) {
+        if (document.getElementById("pantalla").innerText != 0) {
+            let actual = document.getElementById("pantalla").innerText;
+            let concat = document.getElementById("3").innerText;
+            document.getElementById("pantalla").innerText = actual + concat;
+        } else {
+            let actual = "";
+            let concat = document.getElementById("3").innerText;
+            document.getElementById("pantalla").innerText = actual + concat;
+        }
+    }
+}
+
+const write4 = () => {
+
+    if (document.getElementById("pantalla").innerText.length <= 12) {
+        if (document.getElementById("pantalla").innerText != 0) {
+            let actual = document.getElementById("pantalla").innerText;
+            let concat = document.getElementById("4").innerText;
+            document.getElementById("pantalla").innerText = actual + concat;
+        } else {
+            let actual = "";
+            let concat = document.getElementById("4").innerText;
+            document.getElementById("pantalla").innerText = actual + concat;
+        }
+    }
+}
+
+const write5 = () => {
+
+    if (document.getElementById("pantalla").innerText.length <= 12) {
+        if (document.getElementById("pantalla").innerText != 0) {
+            let actual = document.getElementById("pantalla").innerText;
+            let concat = document.getElementById("5").innerText;
+            document.getElementById("pantalla").innerText = actual + concat;
+        } else {
+            let actual = "";
+            let concat = document.getElementById("5").innerText;
+            document.getElementById("pantalla").innerText = actual + concat;
+        }
+    }
+}
+
+const write6 = () => {
+
+    if (document.getElementById("pantalla").innerText.length <= 12) {
+        if (document.getElementById("pantalla").innerText != 0) {
+            let actual = document.getElementById("pantalla").innerText;
+            let concat = document.getElementById("6").innerText;
+            document.getElementById("pantalla").innerText = actual + concat;
+        } else {
+            let actual = "";
+            let concat = document.getElementById("6").innerText;
+            document.getElementById("pantalla").innerText = actual + concat;
+        }
+    }
+}
+
+const write7 = () => {
+
+    if (document.getElementById("pantalla").innerText.length <= 12) {
+        if (document.getElementById("pantalla").innerText != 0) {
+            let actual = document.getElementById("pantalla").innerText;
+            let concat = document.getElementById("7").innerText;
+            document.getElementById("pantalla").innerText = actual + concat;
+        } else {
+            let actual = "";
+            let concat = document.getElementById("7").innerText;
+            document.getElementById("pantalla").innerText = actual + concat;
+        }
+    }
+}
+
+const write8 = () => {
+
+    if (document.getElementById("pantalla").innerText.length <= 12) {
+        if (document.getElementById("pantalla").innerText != 0) {
+            let actual = document.getElementById("pantalla").innerText;
+            let concat = document.getElementById("8").innerText;
+            document.getElementById("pantalla").innerText = actual + concat;
+        } else {
+            let actual = "";
+            let concat = document.getElementById("8").innerText;
+            document.getElementById("pantalla").innerText = actual + concat;
+        }
+    }
+}
+
+const write9 = () => {
+
+    if (document.getElementById("pantalla").innerText.length <= 12) {
+        if (document.getElementById("pantalla").innerText != 0) {
+            let actual = document.getElementById("pantalla").innerText;
+            let concat = document.getElementById("9").innerText;
+            document.getElementById("pantalla").innerText = actual + concat;
+        } else {
+            let actual = "";
+            let concat = document.getElementById("9").innerText;
+            document.getElementById("pantalla").innerText = actual + concat;
+        }
+    }
+}
+
+const write0 = () => {
+
+    if (document.getElementById("pantalla").innerText.length <= 12) {
+        if (document.getElementById("pantalla").innerText != 0) {
+            let actual = document.getElementById("pantalla").innerText;
+            let concat = document.getElementById("0").innerText;
+            document.getElementById("pantalla").innerText = actual + concat;
+        } else {
+            let actual = "";
+            let concat = document.getElementById("0").innerText;
+            document.getElementById("pantalla").innerText = actual + concat;
+        }
+    }
+}
+
+const writePoint = () => {
+
+    if (document.getElementById("pantalla").innerText.length <= 12) {
+        if (document.getElementById("pantalla").innerText != 0) {
+            let actual = document.getElementById("pantalla").innerText;
+            let concat = "."
+            document.getElementById("pantalla").innerText = actual + concat;
+        }
+    }
+}
+
+const writeSum = () => {
+
+    if (document.getElementById("pantalla").innerText.length <= 12) {
+        if (document.getElementById("pantalla").innerText != 0) {
+            let actual = document.getElementById("pantalla").innerText;
+            let concat = "+"
+            document.getElementById("pantalla").innerText = actual + concat;
+        }
+    }
+}
+
+const writeSub = () => {
+
+    if (document.getElementById("pantalla").innerText.length <= 12) {
+        if (document.getElementById("pantalla").innerText != 0) {
+            let actual = document.getElementById("pantalla").innerText;
+            let concat = "-"
+            document.getElementById("pantalla").innerText = actual + concat;
+        }
+    }
+}
+
+const writeMultip = () => {
+
+    if (document.getElementById("pantalla").innerText.length <= 12) {
+        if (document.getElementById("pantalla").innerText != 0) {
+            let actual = document.getElementById("pantalla").innerText;
+            let concat = "*"
+            document.getElementById("pantalla").innerText = actual + concat;
+        }
+    }
+}
+
+const writeDiv = () => {
+
+    if (document.getElementById("pantalla").innerText.length <= 12) {
+        if (document.getElementById("pantalla").innerText != 0) {
+            let actual = document.getElementById("pantalla").innerText;
+            let concat = "/"
+            document.getElementById("pantalla").innerText = actual + concat;
+        }
+    }
+}
+
+const rp = () => {
+
+    if (document.getElementById("pantalla").innerText.length <= 12) {
+        if (document.getElementById("pantalla").innerText != 0) {
+            let actual = document.getElementById("pantalla").innerText;
+            let concat = ")";
+            document.getElementById("pantalla").innerText = actual + concat;
+        } else {
+            let actual = "";
+            let concat = document.getElementById("right-parenthesis").innerText;
+            document.getElementById("pantalla").innerText = actual + concat;
+        }
+    }
+}
+
+const lp = () => {
+
+    if (document.getElementById("pantalla").innerText.length <= 12) {
+        if (document.getElementById("pantalla").innerText != 0) {
+            let actual = document.getElementById("pantalla").innerText;
+            let concat = "(";
+            document.getElementById("pantalla").innerText = actual + concat;
+        } else {
+            let actual = "";
+            let concat = document.getElementById("left-parenthesis").innerText;
+            document.getElementById("pantalla").innerText = actual + concat;
+        }
+    }
+}
+
+const erase = () => {
+
+    if (document.getElementById("pantalla").innerText.length <= 12 && document.getElementById("pantalla").innerText.length > 1) {
+
+        let temp = document.getElementById("pantalla").innerText
+        let actual = "";
+
+        for (let index = 0; index < temp.length - 1; index++) {
+            actual = actual + temp.charAt(index)
+        }
+
+        document.getElementById("pantalla").innerText = actual;
+    } else if (document.getElementById("pantalla").innerText.length == 1) {
+        clearScreen();
+    }
+}
+
+const clearScreen = () => {
+    document.getElementById("pantalla").innerText = "0";
+}
+
+const showResult = () => {
+    try {
+        let expression = document.getElementById("pantalla").innerText;
+        document.getElementById("pantalla").innerText = eval(expression);
+    } catch (error) {
+        document.getElementById("pantalla").innerText = "SINTAX ERROR";
+    }
+}
+
+
+document.getElementById("1").addEventListener("click", write1)
+document.getElementById("2").addEventListener("click", write2)
+document.getElementById("3").addEventListener("click", write3)
+document.getElementById("4").addEventListener("click", write4)
+document.getElementById("5").addEventListener("click", write5)
+document.getElementById("6").addEventListener("click", write6)
+document.getElementById("7").addEventListener("click", write7)
+document.getElementById("8").addEventListener("click", write8)
+document.getElementById("9").addEventListener("click", write9)
+document.getElementById("0").addEventListener("click", write0)
+document.getElementById("sum").addEventListener("click", writeSum)
+document.getElementById("subtraction").addEventListener("click", writeSub)
+document.getElementById("multiplication").addEventListener("click", writeMultip)
+document.getElementById("divide").addEventListener("click", writeDiv)
+document.getElementById("point").addEventListener("click", writePoint)
+document.getElementById("clear").addEventListener("click", clearScreen)
+document.getElementById("equal").addEventListener("click", showResult)
+document.getElementById("left-parenthesis").addEventListener("click", lp)
+document.getElementById("right-parenthesis").addEventListener("click", rp)
+document.getElementById("backspace").addEventListener("click", erase)
